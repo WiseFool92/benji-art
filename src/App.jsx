94fa6c33254f57d7805b744134078307e75a7a0d
Nathan Watkins Hoagland <watkins92@gmail.com>
2021-07-01 10:48:17 -0700

    comment out app.css, removed excess logo assests, framed out my routing system in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path ='/home'>
+          <Home />
+        </Route>
+        <Route path ='/contact'>
+          <Contact />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
