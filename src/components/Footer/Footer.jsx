2ce758c6bd25313126b11ad38fbc23d5fbf040d9
Nathan Watkins Hoagland <watkins92@gmail.com>
2021-07-02 10:19:30 -0700

    Add Footer Background Img, Add Font Awesome accessibility, Create a Footer with a few links

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import background from '../Img/Footer.jpg';
+
+const footer = {
+  display: 'flex',
+  postiion: 'absolute',
+  width: '100%',
+  backgroundImage: `url(${background})`,
+  flexDirection: 'column',
+  alignItems: 'center',
+  textAlign: 'center',
+  justifyContent: 'center',
+  height: '200px',
+  padding: '1rem'
+};
+
+const fabLinkStyle = {
+  color: '#FFFFFF',
+  marginLeft: '1vw',
+  marginRight: '1vw'
+};
+
+const links = {
+  color: '#FFFFFF'
+};
+
+const footerLinks = {
+  float: 'left',
+  color: '#fff'
+};
+
+export default function Footer() {
+  return (
+    <>
+      <div style={footer}>
+        <div style={{marginTop: '1rem', paddingTop: '2rem'}}>
+          <a href="https://github.com/WiseFool92" className="fab fa-github fa-2x" style={fabLinkStyle}> </a>
+          <a href="https://www.linkedin.com/in/nathan-watkins-hoagland/" className="fab fa-linkedin fa-2x" style={fabLinkStyle}> </a>
+          <a href="https://www.instagram.com/mr.supernova/" className="fab fa-instagram fa-2x" style={fabLinkStyle}> </a>
+          <a href="https://www.youtube.com/channel/UCo4USBh5QCeNpv9nvDYoX8Q?view_as=subscriber" className="fab fa-youtube fa-2x" style={fabLinkStyle}> </a>
+        </div>
+      </div>
+    </>
+  )
+}
